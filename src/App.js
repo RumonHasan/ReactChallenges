742e75e52e60f0027a00305892d5c2230535773c
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-02-22 20:03:45 +0900

    memory-game

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import BasicCounterApp from './components/hooks/useState/CounterApp';
 import CodeBlocksApp from './components/CodeEditor/CodeBlocks/CodeBlocksApp';
+// game
+import Memory from './components/Advanced/MemoryGame/Memory';
 
 const App = () => {
   return (
     <div className="apps">
-      <CodeBlocksApp />
+      <Memory />
     </div>
   );
 };
